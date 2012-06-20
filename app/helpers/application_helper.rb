32eea5635740769e795042a50118325cc867d25e
Brad Thompson <brad@bradsotherbrain.com>
2012-06-20 19:22:15 -0400

    Getting it all started

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,80 @@
+module ApplicationHelper
+	def tabify list
+		r = '<ul class="nav nav-tabs">'
+		active = true
+		list.each_pair do |li, str|
+			if content_for? li
+				r << "
+				"
+				if active
+					r << '<li class="active"><a data-toggle="tab" href="#'+li.to_s+'">'+str+'</a></li>'
+					active = false
+				else
+					r << '<li><a data-toggle="tab" href="#'+li.to_s+'">'+str+'</a></li>'
+				end
+			end
+		end
+		r << '</ul>'
+		r << "
+
+		"
+		r << '<div class="tab-content" style="border: thin inset black; margin-top: 0px; background-color: #fff; padding: 5px;">'
+		list.each_pair do |li, str|
+			if content_for? li
+				if !active
+					r << '<div class="tab-pane active" id="'+li.to_s+'">'
+					active = true
+				else
+					r << '<div class="tab-pane" id="'+li.to_s+'">'
+				end
+				r << "
+				"
+				r << content_for(li)
+				r << "
+				"
+				r << '</div>'
+				r << "
+				"
+			end
+		end
+		r << '</div>'
+		r.html_safe
+	end
+
+	def twitterized_type(type)
+		case type
+		when :alert
+			"warning"
+		when :error
+			"error"
+		when :notice
+			"info"
+		when :success
+			"success"
+		else
+			type.to_s
+		end
+	end
+
+	def time_delta time
+		if time < Time.now
+			distance_of_time_in_words_to_now(time) + " ago"
+		else
+			distance_of_time_in_words_to_now(time) + " from now"
+		end
+	end
+
+	def truthy_thumb(value)
+		if value=="*"
+			i('star').html_safe
+		elsif value
+			i('thumbs-up').html_safe
+		else
+			i('thumbs-down').html_safe
+		end
+	end
+
+	def i(c)
+		('<i class="icon-' + c + ' icon-white"></i>').html_safe
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dynamo::Application.config.secret_token = '0643a3253170c297f546910100dfd7b6a0f49bebcc00bc6406fe1d2b652d331bccea50588f0d71934881d67126dd988a5b5ad05217e269a5696989bae6ed75bb'
